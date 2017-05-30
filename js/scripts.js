b5366a45b9bd2ea88572fe84df994f3d20bee3ed
Saul Artiga and diego aguilar <saul+diego@>
2017-05-30 14:07:06 -0700

    create dank ass walrus

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+  $(".walrusclick").click(function() {
+    $("#walrus-showing").fadeToggle();
+    $("#walrus-hidden").fadeToggle();
+  });
+  $(".walrusclick2").click(function() {
+    $("#test-showing").slideToggle();
+    $("#test-hidden").slideToggle();
   });
 });
